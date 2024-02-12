bd5592b8c7e45ed86cb69e53b9af8e07cc4fe166
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-02-12 16:17:08 +0700

    video21

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MyComponent from "./components/MyComponent";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div>
-      Hello world with &amp; Hoi Dan IT
-      <MyComponent></MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>Hello world with Reactjs (Hoi Dan IT)</p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+import { increaseCounter, decreaseCounter } from "./action/actions";
+import MyComponent from "./components/MyComponent";
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello world with &amp; Hoi Dan IT
+        <MyComponent></MyComponent>
+      </div>
+    );
+  }
 }
+
+// function App(props) {
+//   return (
+
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Hello world with Reactjs (Hoi Dan IT)</p>
+//         <div>Count: {props.count}</div>
+
+//         <button onClick={() => props.increaseCounter()}>Increase Count</button>
+
+//         <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+//       </header>
+//     </div>
+//   );
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     count: state.counter.count,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increaseCounter: () => dispatch(increaseCounter()),
+
+//     decreaseCounter: () => dispatch(decreaseCounter()),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,9 +4,46 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Hoi Dan It",
+    address: "Ha noi",
+    age: 26,
+  };
+  handleClick = (event) => {
+    console.log(">>> click my button");
+
+    this.setState({
+      name: "Eric",
+      age: Math.floor(Math.random() * 100 + 1),
+    });
+  };
+
+  handleOnMouseOver(event) {
+    // console.log(event);
+  }
+  handlOnChangeInput = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+  }
   //JSX
   render() {
-    return <div>My first component</div>;
+    return (
+      <div>
+        My name is {this.state.name} and i'm {this.state.age}
+        <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
+          <input
+            type="text"
+            onChange={(event) => { this.handlOnChangeInput(event) }}
+          >
+          </input>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
